8ac86a94ec6b87605f4a0ad111a4acd2df7b27de
JHL71 <neko0717@naver.com>
2023-08-21 21:32:38 +0900

    tet global.css

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,7 @@
 'use client';
-import NavBar from "../NavBar";
 
 export default function Potato() {
   return <div>
-    <NavBar />
     <h1>About</h1>
   </div>
 }
--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
 'use client';
 
-import NavBar from "./NavBar";
-
 export default function Page() {
   return (
     <div>
-      <NavBar />
       <h1 className="active">Hello</h1>
-      <style jsx>{`
-        a {
-          color: white;
-        }
-      `}</style>
     </div>
   )
 }
