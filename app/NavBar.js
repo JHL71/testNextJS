64e577d48b4f2beecb2289f8faea62e1c527fbfe
JHL71 <neko0717@naver.com>
2023-08-21 09:57:31 +0900

    test css modules

--- app/NavBar.js
+++ app/NavBar.js
@@ -1,11 +1,12 @@
 // 'use client';
 import Link from "next/link";
 import {usePathname} from 'next/navigation';
+import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const pathname = usePathname();
-  return <nav>
-    <Link style={{color:pathname === '/' ? 'red' : 'blue'}} href={"/"} className="hello">Home</Link>
-    <Link style={{color:pathname === '/about-us' ? 'red' : 'blue'}} href={"/about-us"}>About</Link>
+  return <nav className={styles.nav}>
+    <Link className={`${styles.link} ${pathname === "/" ? styles.active : ""}`} href={"/"}>Home</Link>
+    <Link className={[styles.link, pathname === "/about" ? styles.active : ""].join(' ')} href={"/about"}>About</Link>
   </nav>
 }
